c2604b06ef26f99a3825536f5fd042cff78d5248
cbouza <44731099+cbouza@users.noreply.github.com>
2018-11-06 12:05:05 -0300

    import de junit para tests, actualizado

--- tests/atenea/fiuba/algoIII/ageoOfEmpires/tests/ArqueroTest.java
+++ tests/atenea/fiuba/algoIII/ageoOfEmpires/tests/ArqueroTest.java
@@ -3,7 +3,7 @@ package atenea.fiuba.algoIII.ageoOfEmpires.tests;
 import atenea.fiuba.algoIII.ageoOfEmpires.Arquero;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 
 public class ArqueroTest {
